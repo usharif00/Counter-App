9441040e57da24a10ff7d77bd82663ae84817d07
usharif00 <126967992+usharif00@users.noreply.github.com>
2024-10-25 22:03:50 +0500

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,24 @@
+
+const decreaseBtn = document.getElementById ("decreaseBtn");
+const increaseBtn = document.getElementById ("increaseBtn");
+const resetBtn = document.getElementById ("resetBtn");
+const countLabel = document.getElementById ("countLabel");
+let count=0;
+
+decreaseBtn.onclick = function (){
+    count--;
+    countLabel.textContent = count;
+
+}
+
+increaseBtn.onclick = function (){
+    count++;
+    countLabel.textContent = count;
+
+}
+
+resetBtn.onclick = function (){
+    count = 0;
+    countLabel.textContent = count;
+
+}
